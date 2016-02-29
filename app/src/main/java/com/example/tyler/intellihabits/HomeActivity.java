4dd486f999513065e36d6adacf9e6018145328cd
Alok Gupta <alok.gupta@shiphawk.com>
2016-02-28 19:43:10 -0800

    added location tracking method

--- app/src/main/java/com/example/tyler/intellihabits/HomeActivity.java
+++ app/src/main/java/com/example/tyler/intellihabits/HomeActivity.java
@@ -6,10 +6,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
     private Button mWaterButton;
@@ -17,10 +19,10 @@ public class HomeActivity extends AppCompatActivity {
     private Button mStudyButton;
     private Button mTrackerButton;
 
-    private static final int REQUEST_CODE_WATER=0;
-    private static final int REQUEST_CODE_GYM=1;
-    private static final int REQUEST_CODE_STUDY=2;
-    private static final int REQUEST_CODE_TRACKER=3;
+    private static final int REQUEST_CODE_WATER = 0;
+    private static final int REQUEST_CODE_GYM = 1;
+    private static final int REQUEST_CODE_STUDY = 2;
+    private static final int REQUEST_CODE_TRACKER = 3;
 
 
 
@@ -28,11 +30,15 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toast.makeText(getApplicationContext(), "Home", Toast.LENGTH_SHORT).show();
+        Log.d("Inside home", "Inside Home");
+
         mWaterButton=(Button)findViewById(R.id.water);
         mWaterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i= WaterActivity.newIntent(HomeActivity.this);
+                Intent i = WaterActivity.newIntent(HomeActivity.this);
                 startActivityForResult(i, REQUEST_CODE_WATER);
 
             }
--- app/src/main/java/com/example/tyler/intellihabits/TrackerActivity.java
+++ app/src/main/java/com/example/tyler/intellihabits/TrackerActivity.java
@@ -2,14 +2,22 @@ package com.example.tyler.intellihabits;
 
 import android.content.Context;
 import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class TrackerActivity extends AppCompatActivity {
+    private static final String TAG = "TrackerActivity";
 
     public static Intent newIntent(Context packageContext){
         Intent i= new Intent(packageContext,TrackerActivity.class);
@@ -19,7 +27,38 @@ public class TrackerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracker);
+        Toast.makeText(getApplicationContext(),"tracker",Toast.LENGTH_SHORT).show();
 
+        //Log.v(TAG, "Inside onCreate");
+        ArrayList<Double> location = getLocation();
+
+        for(Double d: location){
+          //  Log.v(TAG, "Logging locations: ");
+            //Log.v(TAG, " " + d + " ");
+            Toast.makeText(getApplicationContext(),""+d,Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public ArrayList <Double> getLocation()
+    {
+        // Get the location manager
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String bestProvider = locationManager.getBestProvider(criteria, false);
+        Location location = locationManager.getLastKnownLocation(bestProvider);
+        Double lat,lon;
+        ArrayList<Double> aList = new ArrayList<>();
+        try {
+            lat = location.getLatitude ();
+            lon = location.getLongitude();
+            aList.add(lat);
+            aList.add(lon);
+        }
+        catch (NullPointerException e){
+            e.printStackTrace();
+            return null;
+        }
+        return aList;
     }
 
 }
