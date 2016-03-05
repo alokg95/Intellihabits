f69a421ada61148a7f50880309557edf6871db2c
Alok Gupta <alok.gupta@shiphawk.com>
2016-03-04 19:53:51 -0800

    Finished study tracker tool

--- app/src/main/java/com/example/tyler/intellihabits/StudyActivity.java
+++ app/src/main/java/com/example/tyler/intellihabits/StudyActivity.java
@@ -10,6 +10,10 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
@@ -17,10 +21,14 @@ public class StudyActivity extends AppCompatActivity {
 
     private static final String TAG = "StudyActivity";
     public static boolean studySessionToday = false; // Set false initially. Once the user has a study sesh, we'll mark it true.
-    public static int hoursStudied = 0;
+    public static int totalHoursStudied = 0;
+    public static int totalMinutesStudied = 0;
     public static int beginStudySession;
     public static int endStudySession;
+    public static String startTime = "";
+    public static String endTime = "";
     Button btnStudy;
+    TextView studyText;
 
 
 
@@ -35,6 +43,7 @@ public class StudyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_study);
 
+        studyText = (TextView) findViewById(R.id.textView);
         btnStudy = (Button) findViewById(R.id.btnStudy);
         btnStudy.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,11 +52,13 @@ public class StudyActivity extends AppCompatActivity {
             }
         });
 
+        if(studyText.getText().toString().equals("0 hours studied today")){
+            studyText.setText("You've studied exactly 0 hours today. Get studying!");
+        }
+
     }
 
     public void modifyStudySession(){
-//        Calendar calendar = Calendar.getInstance(Locale.getDefault());
-//        int minute = calendar.get(Calendar.MINUTE);
         if(btnStudy.getText().toString().equals("Begin Study Session")){
             beginStudySession();
         } else {
@@ -56,13 +67,50 @@ public class StudyActivity extends AppCompatActivity {
     }
 
     public void beginStudySession(){
-        Log.d(TAG, "Inside beginStudySesh");
         changeButtonState();
+        DateFormat df = new SimpleDateFormat("HH:mm");
+        startTime = df.format(Calendar.getInstance().getTime());
+
+
     }
 
     public void endStudySession(){
-        Log.d(TAG, "Inside endStudySesh");
         changeButtonState();
+        DateFormat df = new SimpleDateFormat("HH:mm");
+        endTime = df.format(Calendar.getInstance().getTime());
+        updateHoursStudied(startTime, endTime);
+    }
+
+    public void updateHoursStudied(String startTime, String endTime){
+        int startMinutes;
+        int endMinutes;
+        int startHour = 0;
+        int endHour = 0;
+        int hoursStudied = 0;
+        int minutesStudied = 0;
+
+        if(startTime.charAt(0) == endTime.charAt(0) && startTime.charAt(1) == endTime.charAt(1)) {
+            // They are within the same hour
+
+            startMinutes = Integer.parseInt(startTime.substring(3,startTime.length()));
+            endMinutes = Integer.parseInt(endTime.substring(3,startTime.length()));
+
+        } else {
+            // They are within different hours. We'll have to calculate the difference
+
+            startHour = Integer.parseInt(startTime.substring(0,2));
+            endHour = Integer.parseInt(endTime.substring(0,2));
+            startMinutes = Integer.parseInt(startTime.substring(3,startTime.length()));
+            endMinutes = Integer.parseInt(endTime.substring(3,startTime.length()));
+
+        }
+        hoursStudied = Math.abs(endHour - startHour);
+        minutesStudied = Math.abs(endMinutes - startMinutes);
+        totalHoursStudied += hoursStudied;
+        totalMinutesStudied += minutesStudied;
+
+        studyText.setText("Studied " + totalHoursStudied + " hours, " + totalMinutesStudied + " minute(s)");
+
     }
 
     public void changeButtonState(){
@@ -78,8 +126,6 @@ public class StudyActivity extends AppCompatActivity {
     public void onStart() {
         super.onStart();
         Log.d(TAG, "onStart fired in " + TAG);
-        hoursStudied++;
-        Log.d(TAG, "Hours Studied Incremented!: " + hoursStudied);
     }
 
 }
